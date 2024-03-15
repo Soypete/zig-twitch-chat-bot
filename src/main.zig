9d49b4bc4e935e326bf170aee2c087654ea9f17f
soypete <petersonmiriah@gmail.com>
2024-03-15 15:22:44 -0600

    point in time auth

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,4 @@
 const std = @import("std");
-var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-const allocator = gpa.allocator();
 
 pub fn main() !void {
     // stdout is for the actual output of your application, for example if you
@@ -9,39 +7,33 @@ pub fn main() !void {
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
+    const allocator = std.heap.page_allocator;
 
     try stdout.print("setting up irc client.\n", .{});
 
     // non-ssl, in real life I think we want ssl
-    // var uri = try std.Uri.parse("ws://irc-ws.chat.twitch.tv:80");
-    var uri = try std.Uri.parse("irc://irc.chat.twitch.tv:6667");
+    const bytes = try allocator.alloc(u8, 100);
+    defer allocator.free(bytes);
 
+    var uri = try std.Uri.parse("https://id.twitch.tv/oauth2/token");
     var client = std.http.Client{
         .allocator = allocator,
     };
-
-    const bytes = try allocator.alloc(u8, 100);
-    defer allocator.free(bytes);
 
     var headers = std.http.Headers{ .allocator = allocator };
     // // does the deinit() remove headers?
     defer headers.deinit();
 
-    try headers.append("accept", "*/*");
+    try headers.append("content-type", "application/x-www-form-urlencoded");
 
-    var req = try client.request(.GET, uri, headers, .{});
-    // does the deinit() remove the variable allocation?
+    var authPayload = "client_id=hof5gwx0su6owfnys0yan9c87zr6t&client_secret=41vpdji4e9gif29md0ouet6fktd2&grant_type=client_credentials";
+    var req = try client.request(.POST, uri, headers, .{});
 
-    try stdout.print("encoding {!}\n", .{req.transfer_encoding});
-    defer req.deinit();
-
-    // run request
     try req.start();
-    // write payload
-    // try req.writer().writeAll("CAP REQ :twitch.tv/membership twitch.tv/tags twitch.tv/commands");
-    // try req.finish();
 
-    // wait for respond
+    // write payload
+    // - why did the example inclued a writer() before writeAll?
+    try req.writer().writeAll(authPayload);
     try req.wait();
 
     // method requires two arguements
@@ -53,38 +45,5 @@ pub fn main() !void {
     // - is defer run at the end of the function
     // - what happens if we get more data than bytes? does streaming via several data packets?
     // - what is the buffer used for?
-    try stdout.print("body {s}\n", .{body});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-// call twitch api auth
-fn doAuth() []u8 {
-    var uri = try std.Uri.parse("https://id.twitch.tv/oauth2/token");
-    var client = std.http.Client{
-        .allocator = allocator,
-    };
-
-    // what does this do?
-    const bytes = try allocator.alloc(u8, 100);
-    defer allocator.free(bytes);
-
-    var headers = std.http.Headers{ .allocator = allocator };
-    // // does the deinit() remove headers?
-    defer headers.deinit();
-
-    try headers.append("content-type", "application/x-www-form-urlencoded");
-
-    var req = try client.request(.GET, uri, headers, .{});
-
-    try req.start;
-
-    return "";
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    try stdout.print("body: {s}\n", .{body});
 }
